b3a5b09635c04c1e027e8389c8e65650e7affe42
BNJ3323 <guitouguitou23@gmail.com>
2025-02-05 15:08:13 +0100

    PlayerMovementAnimation

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -5,6 +5,9 @@ public class PlayerMovement : MonoBehaviour
 {
     private Vector2 moveInput;
     private Player _player;
+    [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer sr;
+
     private void Awake()
     {
         _player = GetComponent<Player>();
@@ -12,13 +15,24 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         transform.position += (Vector3)(moveInput * _player.stats.speed * Time.fixedDeltaTime);
+        animator.SetFloat("Speed", Mathf.Abs(moveInput.x));
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
+        if (context.started)
+        {
+            animator.SetBool("IsMoving", true);
+        }
+
         moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("DirectionX", moveInput.x);
+        animator.SetFloat("DirectionY", moveInput.y);
         if (context.canceled)
         {
+            sr.flipX = moveInput.x < 0 ? true : false;
+            //sr.flipY = moveInput.y < 0 ? true : false;
+            animator.SetBool("IsMoving", false);
             this.enabled = false;
         }
     }
