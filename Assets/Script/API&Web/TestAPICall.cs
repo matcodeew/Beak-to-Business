f37e05cb593c72f8475b7c8ef8930c2c6e3faad0
OwOskan <42584725+Lex0planet@users.noreply.github.com>
2025-02-07 10:51:59 +0100

    Made the TestAPICall script cleaner

--- Assets/Script/API&Web/TestAPICall.cs
+++ Assets/Script/API&Web/TestAPICall.cs
@@ -9,76 +9,81 @@ public class TestAPICall : MonoBehaviour
     //This class contains various example of how to call the API
     [SerializeField] private TextMeshProUGUI debugText;
     [SerializeField] private string url = "http://192.168.1.238/api/api.php?endpoint=users";
-    [SerializeField] private string userid = "22";
+    [SerializeField] private int userid = 22;
     private User _testUser;
 
     void Start()
     {
-        StartCoroutine(InitUser());
+        StartCoroutine(InitUser(userid));
     }
 
-    private IEnumerator InitUser()
+    private IEnumerator InitUser(int id)
     {
-        var task = APICaller.GetUserById(22); // This is better as it makes implementation easier, if problem ever rise you can just change it to TestApiCaller() to test
+        var
+            task = APICaller
+                .GetUserById(
+                    id); // This is better as it makes implementation easier, if problem ever rise you can just change it to TestApiCaller() to test
         yield return new WaitUntil(() => task.IsCompleted);
         _testUser = task.Result;
-        Debug.Log("ID:" + _testUser.id + ", Name:" + _testUser.nickname + ", Email:" + _testUser.email + ", Password:" + _testUser.password);
+        debugText.text = "ID:" + _testUser.id + ", Name:" + _testUser.nickname + ", Email:" + _testUser.email + ", Password:" +
+                  _testUser.password;
     }
+
     private async Task<User> TestApiCaller()
     {
-      return await APICaller.GetUserById(22);
-    }
-    
-    IEnumerator GetUsers(){
-        UnityWebRequest request = UnityWebRequest.Get(url);
-        
-        yield return request.SendWebRequest();
-
-        if (request.result == UnityWebRequest.Result.ConnectionError ||
-            request.result == UnityWebRequest.Result.ProtocolError)
-        {
-            Debug.Log("Erreur ça marche pas au secours" + request.error);
-        }
-        else
-        {
-            string jsonResponse = request.downloadHandler.text;
-            
-            Debug.Log("y'a réponse" + jsonResponse);
-            
-            User[] users = JsonHelper.FromJson<User>(jsonResponse);
-            
-            foreach (User user in users)
-            {
-                Debug.Log("ID:" + user.id + ", Name:" + user.nickname + ", Email:" + user.email + ", Password:" + user.password);
-            }
-            
-        }
-    }
-
-    IEnumerator GetUserFromId()
-    {
-        UnityWebRequest request = UnityWebRequest.Get(url + "/" + userid);
-        
-        yield return request.SendWebRequest();
-
-        if (request.result == UnityWebRequest.Result.ConnectionError ||
-            request.result == UnityWebRequest.Result.ProtocolError)
-        {
-            Debug.Log("YA PAS YA PAS" + request.error);
-        }
-        else
-        {
-            string jsonResponse = request.downloadHandler.text;
-            
-            Debug.Log("TOUCHE TOUCHE" + jsonResponse);
-
-            User user = JsonHelper.FromSingleJson<User>(jsonResponse);
-            
-            debugText.text = "ID:" + user.id + ",<br> Name:" + user.nickname + ",<br> Email:" + user.email + ",<br> Password:" + user.password;
-            Debug.Log("ID:" + user.id + ", Name:" + user.nickname + ", Email:" + user.email + ", Password:" + user.password);
-        }
+        return await APICaller.GetUserById(22);
     }
 }
+
+//IEnumerator GetUsers(){
+    //    UnityWebRequest request = UnityWebRequest.Get(url);
+    //    
+    //    yield return request.SendWebRequest();
+//
+    //    if (request.result == UnityWebRequest.Result.ConnectionError ||
+    //        request.result == UnityWebRequest.Result.ProtocolError)
+    //    {
+    //        Debug.Log("Erreur ça marche pas au secours" + request.error);
+    //    }
+    //    else
+    //    {
+    //        string jsonResponse = request.downloadHandler.text;
+    //        
+    //        Debug.Log("y'a réponse" + jsonResponse);
+    //        
+    //        User[] users = JsonHelper.FromJson<User>(jsonResponse);
+    //        
+    //        foreach (User user in users)
+    //        {
+    //            Debug.Log("ID:" + user.id + ", Name:" + user.nickname + ", Email:" + user.email + ", Password:" + user.password);
+    //        }
+    //        
+    //    }
+    //}
+    //IEnumerator GetUserFromId()
+    //{
+    //    UnityWebRequest request = UnityWebRequest.Get(url + "/" + userid);
+    //    
+    //    yield return request.SendWebRequest();
+//
+    //    if (request.result == UnityWebRequest.Result.ConnectionError ||
+    //        request.result == UnityWebRequest.Result.ProtocolError)
+    //    {
+    //        Debug.Log("YA PAS YA PAS" + request.error);
+    //    }
+    //    else
+    //    {
+    //        string jsonResponse = request.downloadHandler.text;
+    //        
+    //        Debug.Log("TOUCHE TOUCHE" + jsonResponse);
+//
+    //        User user = JsonHelper.FromSingleJson<User>(jsonResponse);
+    //        
+    //        debugText.text = "ID:" + user.id + ",<br> Name:" + user.nickname + ",<br> Email:" + user.email + ",<br> Password:" + user.password;
+    //        Debug.Log("ID:" + user.id + ", Name:" + user.nickname + ", Email:" + user.email + ", Password:" + user.password);
+    //    }
+    //}
+//}
 
 
 
