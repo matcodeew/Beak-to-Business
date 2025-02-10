857f1b13f62d3ac50842072560e27e2cb52f46ab
BNJ3323 <guitouguitou23@gmail.com>
2025-02-10 16:51:19 +0100

    Finish all animation player movement

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -9,12 +9,12 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _moveInput;
     private Player _player;
     [SerializeField] Animator animator;
-    [SerializeField] SpriteRenderer sr;
+    //[SerializeField] SpriteRenderer sr;
     private Vector2 _playerDirection;
 
     [Header("Sprite")]
-    [SerializeField] private Sprite _lastFrame;
-    [SerializeField] private Sprite _defaultSprite;
+    //[SerializeField] private Sprite _lastFrame;
+    //[SerializeField] private Sprite _defaultSprite;
 
     [HideInInspector] public Vector3 direction = Vector2.zero;
     [SerializeField] private Camera _playerCamera;
@@ -46,13 +46,12 @@ public class PlayerMovement : MonoBehaviour
     {
         if (context.started)
         {
-            sr.sprite = _defaultSprite;
+            //sr.sprite = _defaultSprite;
             animator.enabled = true;
             animator.SetBool("IsMoving", true);
         }
 
         _moveInput = context.ReadValue<Vector2>();
-        SetBoolAnimation(_moveInput);
         animator.SetFloat("DirectionX", _moveInput.x);
         animator.SetFloat("DirectionY", _moveInput.y);
 
@@ -63,86 +62,5 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    public void SaveLastFrame()
-    {
-        _lastFrame = sr.sprite;
-    }
-
-    public void SetSprite()
-    {
-        if (_lastFrame == null)
-        {
-            sr.sprite = null;
-            return;
-        }
-        animator.enabled = false;
-        sr.sprite = _lastFrame;
-        _lastFrame = null;
-    }
-
-    #region  ouvrepaspls
-    public void SetBoolAnimation(Vector2 _moveInput)
-    {
-        if ((_moveInput.x > 0 && _moveInput.x < 1) && (_moveInput.y > 0 && _moveInput.y < 1)) // haut droite
-        {
-            print("haut droite");
-            animator.SetBool("MoveUp", true);
-            animator.SetBool("MoveRight", true);
-            animator.SetBool("MoveDown", false);
-            animator.SetBool("MoveLeft", false);
-        }
-        else if ((_moveInput.x < 0 && _moveInput.x > -1) && (_moveInput.y > 0 && _moveInput.y < 1)) // haut gauche
-        {
-            print("haut Gauche");
-            animator.SetBool("MoveUp", true);
-            animator.SetBool("MoveLeft", true);
-            animator.SetBool("MoveDown", false);
-            animator.SetBool("MoveRight", false);
-        }
-        else if ((_moveInput.x > 0 && _moveInput.x < 1) && (_moveInput.y < 0 && _moveInput.y > -1)) // bas droite
-        {
-            print("bas droite");
-            animator.SetBool("MoveDown", true);
-            animator.SetBool("MoveRight", true);
-            animator.SetBool("MoveUp", false);
-            animator.SetBool("MoveLeft", false);
-        }
-        else if ((_moveInput.x < 0 && _moveInput.x > -1) && (_moveInput.y < 0 && _moveInput.y > -1)) // bas gauche
-        {
-            print("bas Gauche");
-            animator.SetBool("MoveDown", true);
-            animator.SetBool("MoveLeft", true);
-            animator.SetBool("MoveUp", false);
-            animator.SetBool("MoveRight", false);
-        }
-        else if (_moveInput == Vector2.up)
-        {
-            animator.SetBool("MoveDown", false);
-            animator.SetBool("MoveLeft", false);
-            animator.SetBool("MoveUp", true);
-            animator.SetBool("MoveRight", false);
-        }
-        else if ( _moveInput == Vector2.down)
-        {
-            animator.SetBool("MoveDown", true);
-            animator.SetBool("MoveLeft", false);
-            animator.SetBool("MoveUp", false);
-            animator.SetBool("MoveRight", false);
-        }
-        else if ( _moveInput == Vector2.right)
-        {
-            animator.SetBool("MoveDown", false);
-            animator.SetBool("MoveLeft", false);
-            animator.SetBool("MoveUp", false);
-            animator.SetBool("MoveRight", true);
-        }
-        else if ( _moveInput == Vector2.left)
-        {
-            animator.SetBool("MoveDown", false);
-            animator.SetBool("MoveLeft", true);
-            animator.SetBool("MoveUp", false);
-            animator.SetBool("MoveRight", false);
-        }
-    }
-    #endregion
+    
 }
