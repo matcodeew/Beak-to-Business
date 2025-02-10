1abe7d399b9a065cf01091c3bd78031e422e579c
Elvin-04 <118186018+Elvin-04@users.noreply.github.com>
2025-02-10 14:59:57 +0100

    attempt to fix errors
    Need to fix shoot

--- Assets/Script/Score/New/GlobalScoreManager.cs
+++ Assets/Script/Score/New/GlobalScoreManager.cs
@@ -19,13 +19,13 @@ public class GlobalScoreManager : MonoBehaviour
         GameObject playerUI = Instantiate(playerScoreTemplate, scoreBoard);
         playerUI.GetComponent<PlayerScore>().TrackPlayer(player, connectionID);
         playerTemplates.Add(playerUI);
-        UpdateOrder();
     }
 
-    // private void Update()
-    // {
-    //     UpdateOrder();
-    // }
+    //Move this
+    private void Update()
+    {
+        UpdateOrder();
+    }
 
     public void UpdateOrder() //call this with event when one of player Update Score
     {
