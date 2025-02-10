5c47bd504e786db9847e5887b76d4eeea02708fa
Kureo524 <kokoshibo524@gmail.com>
2025-02-10 17:32:59 +0100

    Main Menu

---
+++ Assets/Script/MainMenu/APICaller.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public static class APICaller
+{
+    //We make the class static so it can be accessed everywhere at any times
+    //It is also asynchronous since static classes do not support Coroutines
+    private static string api_url = "http://b2b.zapto.org/api/api.php?endpoint=";
+
+    public static async Task<User> GetUserById(int id)
+    {
+        return await GetUserByIdRequest(id);
+    }
+
+    public static async Task<string> GetUserSkinById(int id)
+    {
+        return await GetUserSkinByIdRequest(id);
+    }
+
+
+    //This is just like the coroutine example in the TestAPICall script, except made to be used in a static class
+    private static async Task<User> GetUserByIdRequest(int id)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(api_url + "users/" + id))
+        {
+            var operation = request.SendWebRequest();
+
+            while (!operation.isDone)
+                await Task.Yield();
+
+            if (request.result == UnityWebRequest.Result.ConnectionError ||
+                request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Error: " + request.error);
+                return null;
+            }
+            else
+            {
+                string jsonResponse = request.downloadHandler.text;
+                return JsonHelper.FromSingleJson<User>(jsonResponse);
+            }
+        }
+    }
+    
+    private static async Task<string> GetUserSkinByIdRequest(int _id)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(api_url + "skins/" + _id))
+        {
+            var operation = request.SendWebRequest();
+
+            while (!operation.isDone)
+                await Task.Yield();
+
+            if (request.result == UnityWebRequest.Result.ConnectionError ||
+                request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Error: " + request.error);
+                return null;
+            }
+            else
+            {
+                string jsonResponse = request.downloadHandler.text;
+                return JsonHelper.FromSingleJson<string>(jsonResponse);
+            }
+        }
+    }
+}
+
+
+//These classes are used to format the Json from the api into a User object
+[System.Serializable]
+public class User
+{
+    public int id;
+    public string nickname;
+    public string email;
+    public string password;
+}
+
+public static class JsonHelper
+{
+    public static T[] FromJson<T>(string json)
+    {
+        string newJson = "{\"items\":" + json + "}";
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+        return wrapper.items;
+    }
+  
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] items;
+    }
+    
+    public static T FromSingleJson<T>(string json)
+    {
+        return JsonUtility.FromJson<T>(json);
+    }
+}
---
+++ Assets/Script/MainMenu/SkinsHandler.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinsHandler : MonoBehaviour
+{
+    private string _testUser;
+
+    private Dictionary<string, bool> _skins = new();
+    private Dictionary<string, bool> _hats = new();
+    public List<string> skinsNames;
+    
+    public List<Sprite> skinsSprites;
+    public int skinSelected;
+
+    public Image lilPreview; 
+    public Image bigPreview; 
+    public TextMeshProUGUI skinsNumber;
+    private int _previewNb;
+
+    private int _userId;
+    
+    private void Awake()
+    {
+        for (int i = 0; i < skinsNames.Count; i++)
+        {
+            _skins.Add(skinsNames[i], false);
+        }
+    }
+    public void GetId(int _id)
+    {
+        _userId = _id;
+        Debug.Log(_userId);
+    }
+
+    public void CheckSkin()
+    {
+        StartCoroutine(CheckSkins());
+    }
+
+    public IEnumerator CheckSkins()
+    {
+        var _task = APICaller.GetUserSkinById(_userId);
+        yield return new WaitUntil(() => _task.IsCompleted);
+        _testUser = _task.Result;
+        
+        string[] _userSkins = _testUser.Split(',');
+
+        foreach (string _skin in _userSkins)
+        {
+            if (_skins.ContainsKey(_skin))
+            {
+                _skins[_skin] = true;
+            }
+
+            if (_hats.ContainsKey(_skin))
+            {
+                _hats[_skin] = true;
+            }
+        }
+    }
+
+    public void ChangeSelectedSkin()
+    {
+        if (_skins[skinsNames[_previewNb]])
+        {
+            skinSelected = _previewNb;
+            Debug.Log("skinSelected !");
+            return;
+        }
+        
+        Debug.Log("You don't own this skin !");
+    }
+
+    public void ChangePreviewSkin(int _preview)
+    {
+        _previewNb = _preview;
+        lilPreview.sprite = bigPreview.sprite = skinsSprites[_preview];
+        skinsNumber.text = _preview.ToString();
+    }
+}
