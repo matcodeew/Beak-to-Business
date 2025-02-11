d0dfe6a84168209a8f57cbe55d17bc8f20c46880
Kureo524 <kokoshibo524@gmail.com>
2025-02-11 16:29:58 +0100

    MainMenuWorking

--- Assets/Script/MainMenu/MainMenuScript.cs
+++ Assets/Script/MainMenu/MainMenuScript.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections;
+using System.Runtime.InteropServices;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-    // private User _testUser;
+    private User _testUser;
     
     public TextMeshProUGUI userIdText;
     public TextMeshProUGUI userNameText;
@@ -17,31 +20,48 @@ public class MainMenuScript : MonoBehaviour
     
     public UnityEvent<int> _onUserLoggedIn;
 
-    public void GetUserInfos(int _id)
+    private void Awake()
     {
-        // StartCoroutine(GetUser(_id));
+#if UNITY_WEBGL && !UNITY_EDITOR
+        GetPlayerIdCookie();
+#endif
+        // GetUserId("22");
     }
-    
-    // private IEnumerator GetUser(int _id)
-    // {
-    //     var _task = APICaller.GetUserById(_id); // This is better as it makes implementation easier, if problem ever rise you can just change it to TestApiCaller() to test
-    //     yield return new WaitUntil(() => _task.IsCompleted);
-    //     _testUser = _task.Result;
-    //     
-    //     userId = _testUser.id;
-    //     userName = _testUser.nickname;
-    //     
-    //     SetPlayerInfos();
-    //     
-    //     _onUserLoggedIn.Invoke(userId);
-    //     
-    //     frontPanel.GetComponent<Animator>().SetTrigger("FadeOut");
-    // }
+
+
+    [DllImport("__Internal")]
+    public static extern void GetPlayerIdCookie();
+    public void GetUserId(string _id)
+    {
+        StartCoroutine(GetUser(_id));
+    }
+    private IEnumerator GetUser(string _id)
+    {
+        int _playerId = int.Parse(_id);
+        
+        var _task = APICaller.GetUserById(_playerId); // This is better as it makes implementation easier, if problem ever rise you can just change it to TestApiCaller() to test
+        yield return new WaitUntil(() => _task.IsCompleted);
+        _testUser = _task.Result;
+        
+        userId = _testUser.id;
+        userName = _testUser.nickname;
+        
+        SetPlayerInfos();
+        
+        _onUserLoggedIn.Invoke(userId);
+        
+        frontPanel.GetComponent<Animator>().SetTrigger("FadeOut");
+    }
 
     private void SetPlayerInfos()
     {
         userIdText.text = "Id : " + userId.ToString();
         userNameText.text = "Username : <br>" + userName;
+    }
+
+    public void PlayGame(string _name)
+    {
+        SceneManager.LoadScene(_name);
     }
 
     public void LogOut()
--- Assets/Script/MainMenu/VideoPlayerScript.cs
+++ Assets/Script/MainMenu/VideoPlayerScript.cs
@@ -5,6 +5,7 @@ public class VideoPlayerScript : MonoBehaviour
 {
     public void StartVideo()
     {
+        GetComponent<VideoPlayer>().url = System.IO.Path.Combine (Application.streamingAssetsPath,"Grim.mp4");
         GetComponent<VideoPlayer>().Play();
     }
 }
