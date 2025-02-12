941b143724b8abd9b8b7adba3a1cb66b4647fe87
BNJ3323 <guitouguitou23@gmail.com>
2025-02-12 10:49:55 +0100

    HealthBarFinished

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -3,10 +3,10 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    [SerializeField] private Image _healthFill;
+    
 
-    public void UpdateHealthBar(float currentValue, float maxValue)
-    {
-        _healthFill.fillAmount = currentValue / maxValue;
-    }
+    //public void UpdateHealthBar(float currentValue, float maxValue)
+    //{
+    //    _healthFill.fillAmount = currentValue / maxValue;
+    //}
 }
