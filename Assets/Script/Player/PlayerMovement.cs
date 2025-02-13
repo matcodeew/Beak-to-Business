0ea305d462e2b43fced0683e4a84f5cc785d21b1
BNJ3323 <guitouguitou23@gmail.com>
2025-02-13 17:23:49 +0100

    fix map

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -1,9 +1,11 @@
+
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using Unity.Netcode;
 
-public class PlayerMovement : NetworkBehaviour
+
+public class PlayerMovement : MonoBehaviour
 {
     [Header("Animation")]
     [SerializeField] private Animator _animator;
@@ -19,12 +21,10 @@ public class PlayerMovement : NetworkBehaviour
 
     [Header("Player")]
     private GameObject _currentSkin;
-    private float _playerSpeed = 5f;
+    private float _playerSpeed;
 
     [Header("Mouse")]
     private Vector3 _mousePosition;
-
-    public Vector2 getPlayerInput() => _moveInput;
 
     private void Awake()
     {
@@ -32,20 +32,8 @@ public class PlayerMovement : NetworkBehaviour
         EventManager.OnSkinChanged += SetComponent;
     }
 
-
-    //public override void OnNetworkSpawn()
-    //{
-    //    if (!IsOwner)
-    //    {
-    //        enabled = false;
-    //        return;
-    //    }
-    //}
-
     private void FixedUpdate()
     {
-        if (!IsOwner) return;
-
         _rb.linearVelocity = _moveInput * _playerSpeed;
 
         if (_playerCamera)
@@ -56,11 +44,24 @@ public class PlayerMovement : NetworkBehaviour
             direction = heading / distance;
         }
     }
+    public Vector2 getPlayerInput() => _moveInput;
+    public void GetPlayerSpeed(float Value)
+    {
+        _playerSpeed = Value;
+    }
+    private void SetComponent()
+    {
+        _currentSkin = GetPlayerSkin();
+        _skinAnimation = _currentSkin.GetComponent<PlayerAnimation>();
+    }
+
+    public void SetRightAnimator(GameObject choosenSkin)
+    {
+        _animator = choosenSkin.GetComponent<Animator>();
+    }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        if (!IsOwner) return;
-
         _moveInput = context.ReadValue<Vector2>();
 
         if (context.started)
@@ -69,48 +70,19 @@ public class PlayerMovement : NetworkBehaviour
             _animator.SetBool("IsMoving", true);
         }
 
+        _animator.SetFloat("DirectionX", _moveInput.x);
+        _animator.SetFloat("DirectionY", _moveInput.y);
+        _skinAnimation.SaveLastFrame();
+
         if (context.canceled)
         {
+            //this.enabled = false;
+            
             _moveInput = Vector2.zero;
             _rb.linearVelocity = Vector2.zero;
             _animator.SetBool("IsMoving", false);
+            _skinAnimation.SetSprite();
         }
-
-        UpdateAnimationServerRpc(_moveInput);
-    }
-
-    [ServerRpc]
-    private void UpdateAnimationServerRpc(Vector2 moveInput)
-    {
-        UpdateAnimationClientRpc(moveInput);
-    }
-
-    public void SetPlayerSpeed(float Value)
-    {
-        _playerSpeed = Value;
-    }
-
-    [ClientRpc]
-    private void UpdateAnimationClientRpc(Vector2 moveInput)
-    {
-        if (_animator == null) return;
-
-        _animator.SetFloat("DirectionX", moveInput.x);
-        _animator.SetFloat("DirectionY", moveInput.y);
-
-        bool isMoving = moveInput != Vector2.zero;
-        _animator.SetBool("IsMoving", isMoving);
-    }
-
-    private void SetComponent()
-    {
-        _currentSkin = GetPlayerSkin();
-        if(_currentSkin != null) _skinAnimation = _currentSkin.GetComponent<PlayerAnimation>();
-    }
-
-    public void SetRightAnimator(GameObject choosenSkin)
-    {
-        _animator = choosenSkin.GetComponent<Animator>();
     }
 
     public GameObject GetPlayerSkin()
